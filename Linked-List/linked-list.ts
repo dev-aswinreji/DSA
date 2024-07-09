1b2720d6fcf5dc22a2346f5fd628fb6a242579a0
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-09 23:01:26 +0530

    feat: TS : Linked List Prepend Append Print Insert Remove Node Using Index And Value
    Learning DSA LinkedList using typescript

--- Linked-List/linked-list.ts
+++ Linked-List/linked-list.ts
@@ -9,11 +9,11 @@ class Nodes {
 
 class LinkedList {
   head: null | Nodes;
-  tail: null | Nodes
+  tail: null | Nodes;
   size: number;
   constructor() {
     this.head = null;
-    this.tail = null
+    this.tail = null;
     this.size = 0;
   }
 
@@ -29,7 +29,7 @@ class LinkedList {
     const node = new Nodes(value);
     if (this.isEmpty()) {
       this.head = node;
-      this.tail = node
+      this.tail = node;
     } else {
       node.next = this.head;
       this.head = node;
@@ -37,58 +37,107 @@ class LinkedList {
     this.size++;
   }
 
-  append(value: number){
-    const node = new Nodes(value)
-    if(this.isEmpty()){
-      this.head = node
-      this.tail = node
-    }else{
-      if(this.tail !== null){
-        this.tail.next = node
+  append(value: number) {
+    const node = new Nodes(value);
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      if (this.tail !== null) {
+        this.tail.next = node;
       }
-      this.tail = node
+      this.tail = node;
     }
-    this.size++
+    this.size++;
   }
 
-  // removeNodeUsingIndex(index:number) : number {
-  //   if(this.isEmpty()){
-  //     console.log('value is null');
-  //     return -1
-  //   }
-  //   if(index < 0 || index > this.size){
-  //     console.log('list is empty');
-  //     return 
-  //   }
-  // }
-
-  insertValue(value:number,index:number):number{
-    if(index < 0 || index > this.size){
-      console.log('index is invalid');
-      return index
+  removeNodeUsingIndex(index: number): number {
+    if (index < 0 || index >= this.size) {
+      console.log("list is empty");
+      return -1;
     }
-    const node = new Nodes(value)
-    if(index === 0 ){
-      this.prepend(value)
-    }else{
-      let prev : Nodes | null = this.head
-      for(let i = 0 ; i< index -1 ; i++){
-        if(prev !== null){
-          prev = prev.next
+
+    let removeNode: Nodes | null;
+
+    if (this.size === 1 && index === 0) {
+      this.head = null;
+      this.tail = null;
+    } else if (index === 0) {
+      removeNode = this.head;
+      this.head !== null ? (this.head.next = removeNode) : null;
+    } else if (index === this.size - 1) {
+      let prev: null | Nodes = this.head;
+
+      for (let i = 0; i < index - 1; i++) {
+        if (prev !== null) {
+          prev = prev.next;
         }
       }
-      let nextNode : Nodes | null
-      if(prev !== null){
-        nextNode = prev.next
-        node.next = nextNode
-        prev.next = node
+
+      if (prev !== null && prev.next !== null) {
+        removeNode = prev.next;
+        prev.next = prev.next.next;
+      }
+    }
+    this.size--;
+    return index;
+  }
+
+  insertValue(value: number, index: number): number {
+    if (index < 0 || index > this.size) {
+      console.log("index is invalid");
+      return index;
+    }
+    const node = new Nodes(value);
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      let prev: Nodes | null = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        if (prev !== null) {
+          prev = prev.next;
+        }
+      }
+      let nextNode: Nodes | null;
+      if (prev !== null) {
+        nextNode = prev.next;
+        node.next = nextNode;
+        prev.next = node;
       }
     }
 
-    this.size++
-    return value
+    this.size++;
+    return value;
   }
-  
+
+  removeNodeUsingValue(value: number): number {
+    let removeNode: Nodes | null;
+    if (value === null) {
+      console.log("value is null");
+      return 0;
+    } else if (this.head?.value === value) {
+      removeNode = this.head;
+      this.head.next = removeNode.next;
+      return value;
+    } else {
+      let prev: Nodes | null = this.head;
+      let index: number = 0;
+      console.log(prev,'prev outside while');
+      while (prev?.next && prev.next.value !== value) {
+        prev = prev.next;
+        console.log(prev,'prev')
+        index++;
+      }
+      console.log(prev,'abouve if statement');
+       if(prev !== null && prev.next){
+          prev.next = prev.next.next
+          return index
+        }
+      
+      console.log("value not found");
+      return -1;
+    }
+  }
 
   print(): void {
     if (this.isEmpty()) {
@@ -112,10 +161,15 @@ list.print();
 
 list.prepend(4);
 
-list.prepend(6)
+list.prepend(6);
 
-list.append(3)
+list.append(3);
 
-list.insertValue(2,5)
+list.insertValue(2, 5);
+
+list.removeNodeUsingIndex(2);
+
+
+list.removeNodeUsingValue(4)
 
 list.print();
